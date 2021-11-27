ded67602efe22c37467214bcdb381871ca9406b4
Masikur Rahman <misuk@businessaccelerate.com.bd>
2021-11-28 01:36:54 +0600

    add pair bonus

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\DailyBonus::class,
+        Commands\PairBonus::class,
     ];
 
     /**
@@ -28,6 +29,8 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('bonus:daily')->weekdays()
             ->daily();
+        $schedule->command('pairbonus:daily')->weekdays()
+            ->dailyAt('13.00');
     }
 
     /**
